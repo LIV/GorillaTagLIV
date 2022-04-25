9cb1b97807b1b79df52382a20f2f992730aa774a
Raicuparta <raicuparta@gmail.com>
2022-04-25 10:37:54 +0200

    Initial commit

---
+++ GorillaTagLIV/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GorillaTagLIV
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.raicuparta.gorillatag.gorillatagliv";
+		public const string Name = "GorillaTagLIV";
+		public const string Version = "1.0.0";
+	}
+}
