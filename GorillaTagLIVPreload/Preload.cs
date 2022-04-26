e0ed928705c70af5b5f3b945bc470d6ad056f0db
Raicuparta <raicuparta@gmail.com>
2022-04-26 13:12:34 +0200

    WIP add preload patcher that copies LIV plugin

---
+++ GorillaTagLIVPreload/Preload.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Mono.Cecil;
+
+namespace GorilaTagLIVInstaller
+{
+    public static class Preload
+    {
+        public static IEnumerable<string> TargetDLLs { get; } = new[] {"Assembly-CSharp.dll"};
+
+        public static void Patch(AssemblyDefinition assembly)
+        {
+            var executablePath = Assembly.GetExecutingAssembly().Location;
+            var patchersPath = Path.GetDirectoryName(executablePath);
+            var pluginsPath = Path.Combine(patchersPath, "../../../Gorilla Tag_Data/Plugins/x86_64/LIV_Bridge.dll");
+        
+            File.Copy(Path.Combine(patchersPath, "LIV_Bridge.dll"), pluginsPath);
+        }
+    }
+}
